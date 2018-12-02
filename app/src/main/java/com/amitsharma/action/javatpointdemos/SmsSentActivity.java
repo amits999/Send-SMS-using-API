04fc19e9d1e2e58dea04bfb0c25594a08276090c
AMitSharma <awesomeamit50@gmail.com>
2018-12-02 21:20:57 +0530

    Initial Commit

---
+++ app/src/main/java/com/amitsharma/action/javatpointdemos/SmsSentActivity.java
@@ -0,0 +1,13 @@
+package com.amitsharma.action.javatpointdemos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmsSentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_sent);
+    }
+}
